61db4eed924230b1cfc060addb7f02af3330bd2e
Gaurav Patel <gp43883@gmail.com>
2023-10-23 09:15:21 +0530

    db solving try

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require("dotenv").config()
 
 const mongo = ()=>{
-     mongoose.connect(`${process.env.CONN_STR}`, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+     mongoose.connect(`${process.env.DB_CONNECT}`, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("DB connected")
   }).catch((err)=>console.log(err));
 }
