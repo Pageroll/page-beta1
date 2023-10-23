d9b14ee330599cec76466db4cf0fb51570143142
Gaurav Patel <gp43883@gmail.com>
2023-10-23 12:04:30 +0530

    new Chnagee22

--- Models/UserFiles.js
+++ Models/UserFiles.js
@@ -32,7 +32,7 @@ const UserSchema = new Schema ({
 })
 UserSchema.methods.generateAuthToken=async function(){
     try{
-        const token = jwt.sign({_id:this._id},process.env.SECRET)
+        const token = jwt.sign({_id:this._id},"process.env.SECRET")
         this.tokens = this.tokens.concat({token:token})
         await this.save() ; 
         return token ; 
