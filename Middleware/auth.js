20576730e4016b97c638a2df2af3fc147020977f
Gaurav Patel <gp43883@gmail.com>
2023-10-23 12:53:17 +0530

    Done2

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,35 +1,36 @@
 const userSchema = require('../Models/UserFiles')
-const dotenv = require('dotenv') ;
+const dotenv = require('dotenv');
 dotenv.config();
 const jwt = require('jsonwebtoken')
 const user2022 = require('../Models/User.js')
-const auth = async(req, res, next)=>{
-    try{
-        console.log("auth reached") ;
-        const token =await req.cookies.jwt
-        const verifyUser = jwt.verify(token,"process.env.SECRET") ;  
-       // console.log(verifyUser);
-        const user = await userSchema.findOne({_id:verifyUser._id,"tokens.token":token}) ; 
-        const user2 = await user2022.findOne({_id:verifyUser._id,"tokens.token":token}) ;   
-        if(!user && !user2){throw new Error('User Not Found')}
-       else if(user){ 
-        req.token = token ; 
-        req.user = user ; 
-        req.userID = user._id ; 
-        console.log(user.Name)
-        next();
-       }
-       else if(user2){ 
-        req.token = token ; 
-        req.user = user2 ; 
-        req.userID = user2._id ; 
-        console.log(user2.Name)
-        next();
-       }
+const auth = async (req, res, next) => {
+    try {
+        console.log("auth reached");
+        const token = await req.cookies.jwt
+        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(verifyUser);
+        console.log("SECRET_KEY:", process.env.SECRET_KEY)
+        const user = await userSchema.findOne({ _id: verifyUser._id, "tokens.token": token });
+        const user2 = await user2022.findOne({ _id: verifyUser._id, "tokens.token": token });
+        if (!user && !user2) { throw new Error('User Not Found') }
+        else if (user) {
+            req.token = token;
+            req.user = user;
+            req.userID = user._id;
+            console.log(user.Name)
+            next();
+        }
+        else if (user2) {
+            req.token = token;
+            req.user = user2;
+            req.userID = user2._id;
+            console.log(user2.Name)
+            next();
+        }
     }
-    catch(error){
+    catch (error) {
         res.status(401).send("No token provded");
-        console.log(error) ;
+        console.log(error);
     }
 }
 
