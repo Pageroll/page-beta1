d55b444126607ec5258fd491cdded0660ef92d6c
Gaurav Patel <gp43883@gmail.com>
2023-10-24 15:48:52 +0530

    tryNew

--- index.js
+++ index.js
@@ -118,13 +118,13 @@ app.get('/',auth, (req, res) => {
 
 app.get('/logout',(req,res)=>{
     console.log("fetched logout")
-    res.clearCookie('jwt','',{
-        path: '/',
-        sameSite: 'none',
-        overwrite: true,
-    });
-    res.status(200).send('User Logout');
-    
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 5 * 1000),
+        httpOnly: true,
+    })
+    res
+        .status(200)
+        .json({ success: true, message: 'User logged out successfully' })    
 })
 
 app.listen(PORT, () => {
